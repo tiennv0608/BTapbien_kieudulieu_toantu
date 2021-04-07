f9ad566dfc09ea46de6fad2c133b7cc94ed03031
tiennv0608 <tiennv0608@gmail.com>
2021-04-07 20:16:39 +0700

    baitap

--- 03. BT_ungdungmaytinh/ungdungmaytinh.js
+++ 03. BT_ungdungmaytinh/ungdungmaytinh.js
@@ -4,7 +4,7 @@ function clickButton1() {
     let secondNumber = document.getElementById('secondNumber').value;
     let b = parseFloat(secondNumber);
     let v;
-    alert(v=a+b);
+    document.getElementById('results').innerHTML=('Result Division : '+(v=a+b));
 }
 function clickButton2() {
     let firstNumber = document.getElementById('firstNumber').value;
@@ -12,18 +12,20 @@ function clickButton2() {
     let secondNumber = document.getElementById('secondNumber').value;
     let b = parseFloat(secondNumber);
     let v;
-    alert(v=a-b);}
+    document.getElementById('results').innerHTML=('Result Division : '+(v=a-b));
+}
 function clickButton3() {
     let firstNumber = document.getElementById('firstNumber').value;
     let a = parseFloat(firstNumber);
     let secondNumber = document.getElementById('secondNumber').value;
     let b = parseFloat(secondNumber);
     let v;
-    alert(v=a*b);}
+    document.getElementById('results').innerHTML=('Result Division : '+(v=a*b));
+}
 function clickButton4() {
     let firstNumber = document.getElementById('firstNumber').value;
     let a = parseFloat(firstNumber);
     let secondNumber = document.getElementById('secondNumber').value;
     let b = parseFloat(secondNumber);
     let v;
-    alert(v=a/b);}
+    document.getElementById('results').innerHTML=('Result Division : '+(v=a/b));}
